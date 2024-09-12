02b2aba2029d3824b4a64c8b0db6a64000191dd7
shin <shin@onlyperps.com>
2024-09-12 15:49:06 +0800

    script to configure oracle tokens

---
+++ scripts/configureOracleTokens.ts
@@ -0,0 +1,59 @@
+import hre from "hardhat";
+
+import { expandDecimals } from "../utils/math";
+import * as keys from "../utils/keys";
+import { setAddressIfDifferent, setBytes32IfDifferent, setUintIfDifferent } from "../utils/dataStore";
+
+async function main() {
+  const oracleConfig = await hre.gmx.getOracle();
+  const tokens = await hre.gmx.getTokens();
+
+  if (oracleConfig) {
+    for (const tokenSymbol of Object.keys(oracleConfig.tokens)) {
+      const token = tokens[tokenSymbol];
+      if (!token) {
+        throw new Error(`Missing token for ${tokenSymbol}`);
+      }
+      const { priceFeed, oracleType } = oracleConfig.tokens[tokenSymbol];
+
+      const oracleTypeKey = keys.oracleTypeKey(token.address);
+      await setBytes32IfDifferent(oracleTypeKey, oracleType, "oracle type");
+
+      if (!priceFeed) {
+        continue;
+      }
+
+      const priceFeedAddress = priceFeed.deploy
+        ? (await hre.ethers.getContract(`${tokenSymbol}PriceFeed`)).address
+        : priceFeed.address;
+
+      const priceFeedKey = keys.priceFeedKey(token.address);
+      await setAddressIfDifferent(priceFeedKey, priceFeedAddress, `${tokenSymbol} price feed`);
+
+      const priceFeedMultiplierKey = keys.priceFeedMultiplierKey(token.address);
+      const priceFeedMultiplier = expandDecimals(1, 60 - priceFeed.decimals - token.decimals);
+      await setUintIfDifferent(priceFeedMultiplierKey, priceFeedMultiplier, `${tokenSymbol} price feed multiplier`);
+
+      if (priceFeed.stablePrice) {
+        const stablePriceKey = keys.stablePriceKey(token.address);
+        const stablePrice = priceFeed.stablePrice.div(expandDecimals(1, token.decimals));
+        await setUintIfDifferent(stablePriceKey, stablePrice, `${tokenSymbol} stable price`);
+      }
+
+      await setUintIfDifferent(
+        keys.priceFeedHeartbeatDurationKey(token.address),
+        priceFeed.heartbeatDuration,
+        `${tokenSymbol} heartbeat duration`
+      );
+    }
+  }
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((ex) => {
+    console.error(ex);
+    process.exit(1);
+  });
